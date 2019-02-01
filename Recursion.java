6bc84ef2cfd814c99f5ee61c987385481b486544
clee010922 <clee01@stuy.edu>
2019-01-31 21:09:30 -0500

    completed tailed recursion version of fibonacci. Wrote main method to confirm that it works

--- Recursion.java
+++ Recursion.java
@@ -6,9 +6,13 @@ public class Recursion {
      *precondition: n is non-negative
 
     */
+
+    /*
     public static double sqrt(double n, double tolerance){
 
     }
+
+    */
 
     /*Recursively find the n'th fibbonaci number in linear time
      *fib(0) = 1; fib(1) = 1; fib(5) = 5
@@ -18,10 +22,30 @@ public class Recursion {
       return fib(n, 0, 1);
     }
 
-    
+    private static int fib(int n, int a, int b) {
+      if (n == 0)
+        return a;
+      if (n == 1)
+        return b;
+      return fib(n-1, b, a + b);
+    }
 
-    /*As Per classwork*/
+
+
+    /*As Per classwork
     public static ArrayList<Integer> makeAllSums(){
+    }
+    */
+
+
+    public static void main(String[] args) {
+      System.out.println(fib(0));
+      System.out.println(fib(1));
+      System.out.println(fib(2));
+      System.out.println(fib(3));
+      System.out.println(fib(4));
+      System.out.println(fib(5));
+      System.out.println(fib(6));
     }
 
 }
